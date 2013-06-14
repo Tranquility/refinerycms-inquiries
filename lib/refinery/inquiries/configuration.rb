9df0e562249f122a218b469f8766d65c6beb7812
Uģis Ozols <ugis.ozolss@gmail.com>
2013-06-14 16:33:02 +0300

    Add config which allows to configure "from" name in email notifications.

--- lib/refinery/inquiries/configuration.rb
+++ lib/refinery/inquiries/configuration.rb
@@ -2,12 +2,12 @@ module Refinery
   module Inquiries
     include ActiveSupport::Configurable
 
-    config_accessor :show_contact_privacy_link
-    config_accessor :show_phone_number_field
-    config_accessor :show_placeholders
+    config_accessor :show_contact_privacy_link, :show_phone_number_field,
+                    :show_placeholders, :from_name
 
     self.show_contact_privacy_link = true
     self.show_phone_number_field = true
     self.show_placeholders = true
+    self.from_name = "no-reply"
   end
 end
